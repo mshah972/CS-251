62bf6599bfefd01f1a735e7e4b60e53d21768e25
smoksh17 <73042339+smoksh17@users.noreply.github.com>
2023-01-15 15:06:58 -0600

    Integrated all the necessary files for program 1

---
+++ Programs/Program 1/main.cpp
@@ -0,0 +1,16 @@
+//
+//  Project 1 Starter Code - Gerrymandering
+//  TODO:  Replace this comment with your header info.
+//  
+//
+
+#include "ourvector.h"
+
+using namespace std;
+
+int main() {
+    //TODO:  Write your code here.  
+    // You should have LOTS of function decomposition.
+    
+    return 0;
+}
