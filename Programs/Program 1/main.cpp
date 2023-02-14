328a55e4f9310f70cdb43cf65b033c282f1da7f1
smoksh17 <73042339+smoksh17@users.noreply.github.com>
2023-02-14 00:25:34 -0600

    Working Program 2 So far

--- Programs/Program 1/main.cpp
+++ Programs/Program 1/main.cpp
@@ -14,9 +14,41 @@
 
 using namespace std;
 
-int main() {
-    //TODO:  Write your code here.  
-    // You should have LOTS of function decomposition.
-    
+int MAX_CHARACTERS = 150;
+
+bool data_loaded = false;
+string state = "N/A";
+string command = " ";
+string is_data_loaded = " ";
+
+void display_prompt()
+{
+    if (data_loaded == true)
+    {
+        is_data_loaded = "Yes";
+    }
+    else
+    {
+        is_data_loaded = "No";
+    }
+
+    cout << "Data loaded? " << is_data_loaded << endl;
+    cout << "State: " << state << endl;
+    cout << endl;
+
+    cout << "Enter a command: ";
+    getline(cin, command);
+    cout << endl;
+}
+
+int main()
+{
+    // TODO:  Write your code here.
+    //  You should have LOTS of function decomposition.
+
+    display_prompt();
+
+    cout << "Here is the command you entered: " << command << endl;
+
     return 0;
 }
